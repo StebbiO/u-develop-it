2df61dcd3813b46b187588ce0f22133b85b623ac
Stebbi <stefodd@yahoo.com>
2022-04-19 16:52:17 -0500

    first db query

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // mysql username
+        user: 'root',
+        password: 'p5-H\\"H>.#%gR>Uj4',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
 // default response for any other request (Not found)
